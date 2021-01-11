d2ecd1d8f35704e3258aa0dda8c90a3c138dca81
Olga <73897007+brizolya@users.noreply.github.com>
2021-01-11 13:55:33 +0100

    Add new class for login-page and change main-class

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,8 @@
 from selenium.webdriver.common.by import By
 
-
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    REG_LINK = (By.CSS_SELECTOR, "#registration_link")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
