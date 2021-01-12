d00e23b0327f03d6f17a7c2bf0da272cd42c3664
Olga <73897007+brizolya@users.noreply.github.com>
2021-01-12 11:38:23 +0100

    change functions should_be_login_url and should_beregister_form

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,17 +1,18 @@
 from .base_page import BasePage
-from selenium.webdriver.remote.webdriver import WebDriver
 from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
 
     def should_be_login_url(self):
+        #driver = WebDriver()
         # реализуйте проверку на корректный url адрес
-        assert "login" in self.WebDriver.current_url, "URL-adress is not correct"
+        assert "login" in self.url, "URL-adress is not correct"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
@@ -19,4 +20,4 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*LoginPageLocators.REG_LINK)
+        assert self.is_element_present(*LoginPageLocators.REG_LINK), "There is no registration form"
