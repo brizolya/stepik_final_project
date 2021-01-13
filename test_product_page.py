76c73410827a63251fff8e4cb43f3efb3139181b
Olga <73897007+brizolya@users.noreply.github.com>
2021-01-13 11:36:18 +0100

    test-case for check adding product to basket

---
+++ test_product_page.py
@@ -0,0 +1,13 @@
+from .pages.product_page import ProductPage
+import time
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_item_to_basket()
+    page.solve_quiz_and_get_code()
+    page_with_messages = ProductPage(browser, browser.current_url)
+    time.sleep(3)
+    page_with_messages.is_element_in_basket()
+    page_with_messages.is_item_price_in_basket()
