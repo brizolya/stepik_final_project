969cc32ea761e9bb269e9afaee0cfdd63a27ef62
Olga <73897007+brizolya@users.noreply.github.com>
2021-01-13 11:34:54 +0100

    add new locators for product page

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     REG_LINK = (By.CSS_SELECTOR, "#register_form")
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_form")
+
+class ProductPageLocators():
+    BTN_ADD_TO_BSK = (By.CLASS_NAME, "btn-add-to-basket")
+    MESS_SUCCESS = (By.CSS_SELECTOR, ".alertinner > strong")
+    TITLE_OF_BOOK = (By.CSS_SELECTOR, ".product_main > h1")
+    PRICE_IN_MESS = (By.CSS_SELECTOR, ".alertinner > p > strong")
+    PRICE_IN_ITEM = (By.CSS_SELECTOR, ".product_main > .price_color")
