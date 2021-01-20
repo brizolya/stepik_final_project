ede09a65a93de7440069baea0e9decb30ed85699
Olga <73897007+brizolya@users.noreply.github.com>
2021-01-20 13:46:43 +0100

    add two new test: guest_can_go_to_login_page_from_product_page and guest_should_see_login_link_on_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -12,13 +12,24 @@ import pytest
                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
+def test_guest_can_add_product_to_basket(browser,link): 
     #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.add_item_to_basket()
     page.solve_quiz_and_get_code()
     page_with_messages = ProductPage(browser, browser.current_url, 5)
-    #page.browser.implicitly_wait(5)
     page_with_messages.is_element_in_basket()
     page_with_messages.is_item_price_in_basket()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
