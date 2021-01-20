a59f8b3cb379b8a101b29b6c2689c5be17c14d17
Olga <73897007+brizolya@users.noreply.github.com>
2021-01-20 15:08:52 +0100

    add new test_guest_can_see_product_in_basket_opend_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 import time
 import pytest
 
@@ -33,3 +34,12 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/hacking-exposed-wireless_208/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page_basket = BasketPage(browser, browser.current_url)
+    page_basket.is_basket_empty()
+    page_basket.is_message_empty_basket()
