c32d12821b1c057d649c115ff899d1d867ee3c96
Olga <73897007+brizolya@users.noreply.github.com>
2021-01-20 15:08:02 +0100

    change test_guest_can_go_to_login_page and add new test_guest_can_see_product_in_basket_opened_from_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,23 @@
 from .pages.main_page import MainPage
+from .pages.basket_page import BasketPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    login_page = page.go_to_login_page()
-    login_page.should_be_login_page()
+    page.go_to_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page_basket = BasketPage(browser, browser.current_url)
+    page_basket.is_basket_empty()
+    page_basket.is_message_empty_basket()
