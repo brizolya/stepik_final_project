00fac6c4b59cf975f14d6b770459758103a0ef51
Olga <73897007+brizolya@users.noreply.github.com>
2021-01-20 16:26:57 +0100

    add new method register_new_user

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
+from selenium.webdriver.common.keys import Keys
 
 
 class LoginPage(BasePage):
@@ -21,3 +22,12 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REG_LINK), "There is no registration form"
+    
+    def register_new_user(self, email, password):
+        reg_email = self.browser.find_element(*LoginPageLocators.REGISTR_EMAIL_INPUT)
+        reg_email.send_keys(email)
+        reg_pass = self.browser.find_element(*LoginPageLocators.REGISTR_PASS)
+        reg_pass.send_keys(password)
+        reg_pass_conf = self.browser.find_element(*LoginPageLocators.REGISTR_PASS_CONFIRM)
+        reg_pass_conf.send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTR_SUBM_BUTT).click()
